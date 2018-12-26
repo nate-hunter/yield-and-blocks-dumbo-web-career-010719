9111fe74baab9faea8eb22d6bce5646a3637404c
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-26 16:37:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,22 @@
-def hello_t
-
+=begin 
+def hello_t (names)
+  names.each { |name|
+  puts "#{name}"
+  }
 end
+=end 
 
 # call your method here!
+
+def hello_t(array)
+  if block_given?   # The block_given? method returns true if the method that contains block_given? is called with a block and false if it is not.
+  i = 0   # Counter variable 
+  
+  while i < array.length
+    yield(array[i])
+    i = i + 1 
+  end
+  array 
+end
+
 
